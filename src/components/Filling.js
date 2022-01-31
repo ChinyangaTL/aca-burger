ca53325645dd8fbf842551ab42428e9f535d8825
Les Chinyanga <les.chinyanga@africacode.academy>
2022-01-31 18:45:24 +0200

    Implement filling vanish on click

--- src/components/Filling.js
+++ src/components/Filling.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Filling = ({ type, backgroundColor }) => {
+  const [isVisible, setIsVisible] = useState(true);
+  const makeVanish = () => {
+    setIsVisible(false);
+  };
   return (
-    <div className="burger-piece" style={{ backgroundColor }}>
+    <div
+      data-i
+      onClick={makeVanish}
+      className="burger-piece"
+      style={{
+        backgroundColor: `${backgroundColor}`,
+        display: `${isVisible ? '' : 'none'}`,
+      }}
+    >
       <h3>{type}</h3>
     </div>
   );
