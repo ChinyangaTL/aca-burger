111ca806c7cc4d385727b812571efcd5fb888772
Les Chinyanga <les.chinyanga@africacode.academy>
2022-01-31 16:51:45 +0200

    Initial structure

---
+++ src/components/BaseBread.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BaseBread = () => {
+  return (
+    <div>
+      <h1>base bread</h1>
+    </div>
+  );
+};
+
+export default BaseBread;
---
+++ src/components/TopBread.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopBread = () => {
+  return (
+    <div>
+      <h1>top bread</h1>
+    </div>
+  );
+};
+
+export default TopBread;
