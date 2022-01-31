368d1ca4a42706ff8ad228b385562849bcd2e0c5
Les Chinyanga <les.chinyanga@africacode.academy>
2022-01-31 17:22:41 +0200

    Fix attribution

---
+++ src/components/Attribution.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Attribution = () => {
+  return (
+    <div>
+      <p>
+        Built by <a href="https://github.com/ChinyangaTL">Les Chinyanga</a> for
+        Africa Code Academy
+      </p>
+    </div>
+  );
+};
+
+export default Attribution;
