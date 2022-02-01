5863b0668d3031b06f3ddd70de5600ed6abea905
Les Chinyanga <les.chinyanga@africacode.academy>
2022-02-01 20:42:34 +0200

    Add animation to attribution

--- src/components/Attribution.js
+++ src/components/Attribution.js
@@ -1,13 +1,18 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 
 const Attribution = () => {
   return (
-    <div>
+    <motion.div
+      initial={{ y: '-100vh' }}
+      animate={{ y: 0 }}
+      transition={{ type: 'spring', stiffness: 120, delay: 2 }}
+    >
       <p>
         Built by <a href="https://github.com/ChinyangaTL">Les Chinyanga</a> for
         Africa Code Academy
       </p>
-    </div>
+    </motion.div>
   );
 };
 
