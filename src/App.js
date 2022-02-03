05c56438e892d624f4c4e84336960a5ca635f514
Les Chinyanga <les.chinyanga@africacode.academy>
2022-02-03 12:46:48 +0200

    Add unique id for each filling layer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Attribution from './components/Attribution';
 import BaseBread from './components/BaseBread';
 import Filling from './components/Filling';
 import TopBread from './components/TopBread';
 import { motion } from 'framer-motion';
+import nodeid from 'node-id';
 
 const initialFilling = [
-  { type: 'Tomato', backgroundColor: 'red' },
-  { type: 'Meat', backgroundColor: '#4e1700' },
-  { type: 'Lettuce', backgroundColor: 'green' },
+  { id: nodeid(), type: 'Tomato', backgroundColor: 'red' },
+  { id: nodeid(), type: 'Meat', backgroundColor: '#4e1700' },
+  { id: nodeid(), type: 'Lettuce', backgroundColor: 'green' },
 ];
+
+const FillingPicker = () => {
+  return <div>filling picker</div>;
+};
 
 function App() {
   const [filling, setFilling] = useState(initialFilling);
+  useEffect(() => {
+    console.log(filling);
+  });
   return (
     <motion.div className="App">
       <TopBread styling={breadStyles} />
       {filling.map((layer) => {
-        const { type, backgroundColor } = layer;
-        return <Filling type={type} backgroundColor={backgroundColor} />;
+        const { id, type, backgroundColor } = layer;
+        return (
+          <Filling key={id} type={type} backgroundColor={backgroundColor} />
+        );
       })}
       <BaseBread styling={breadStyles} />
       <Attribution />
+      <FillingPicker />
     </motion.div>
   );
 }
