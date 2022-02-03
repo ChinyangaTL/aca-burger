bf439978b53ade45f2fe9db2e09c137d154d5ac9
Les Chinyanga <les.chinyanga@africacode.academy>
2022-02-03 12:31:33 +0200

    Use iterable filling state for fillings instead of hardcoding components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Attribution from './components/Attribution';
 import BaseBread from './components/BaseBread';
@@ -5,13 +6,21 @@ import Filling from './components/Filling';
 import TopBread from './components/TopBread';
 import { motion } from 'framer-motion';
 
+const initialFilling = [
+  { type: 'Tomato', backgroundColor: 'red' },
+  { type: 'Meat', backgroundColor: '#4e1700' },
+  { type: 'Lettuce', backgroundColor: 'green' },
+];
+
 function App() {
+  const [filling, setFilling] = useState(initialFilling);
   return (
     <motion.div className="App">
       <TopBread styling={breadStyles} />
-      <Filling type="Tomato" backgroundColor="red" />
-      <Filling type="Meat" backgroundColor="#4e1700" />
-      <Filling type="Lettuce" backgroundColor="green" />
+      {filling.map((layer) => {
+        const { type, backgroundColor } = layer;
+        return <Filling type={type} backgroundColor={backgroundColor} />;
+      })}
       <BaseBread styling={breadStyles} />
       <Attribution />
     </motion.div>
